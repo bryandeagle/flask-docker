243e12c7bfe162c907ce9bfa311384bc96e4dd93
Bryan Deagle <bryan.deagle@ni.com>
2020-08-14 22:37:57 -0500

    myapp

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-docker rm -f fetch
+docker rm -f myapp
 docker build -t deagle/myapp:stable .
 docker run --init --name="myapp" --env IP=172.18.0.2 --restart always -d -p 8080:5000 deagle/myapp:stable
