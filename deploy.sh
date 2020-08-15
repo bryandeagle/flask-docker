546494bd3e3d90a257862fefc61054d29563385e
Bryan Deagle <bryan.deagle@ni.com>
2020-08-14 22:47:19 -0500

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 docker rm -f myapp
 docker build -t deagle/myapp:stable .
-docker run --init --name="myapp" --env IP=172.18.0.2 --restart always -d -p 8080:5000 deagle/myapp:stable
+# docker run --init --name="myapp" --ip 172.18.0.3 --env IP=172.18.0.3 --restart always -d -p 8080:5000 deagle/myapp:stable
+docker run --init --name="myapp" --network="fetch-net"  --ip 172.18.0.3 --env IP=172.18.0.3 --restart always -d -p 5150:5000 deagle/myapp:stable
